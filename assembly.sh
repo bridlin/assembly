ce834eef50746561be755794ba68cc25157d3e9b
bridlin.barckmann <bridlinbarckmann@gmail.com>
2024-12-05 12:05:07 +0100

    added spades

--- assembly.sh
+++ assembly.sh
@@ -29,26 +29,27 @@ mkdir $output_dir
 
 for sample in "${input_list[@]}"; do
 echo $sample &&
-fastqc $fastq_directory/$sample\L001_R1_001.fastq.gz \
+fastqc $fastq_directory/$sample\1.fastq.gz \
     --outdir $output_dir &&
-fastqc $fastq_directory/$sample\L001_R2_001.fastq.gz \
+fastqc $fastq_directory/$sample\2.fastq.gz \
     --outdir $output_dir &&
 cutadapt  -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA   -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
-    -o $fastq_directory/$sample\L001_R1_001_3trimmed.fastq.gz \
-    -p $fastq_directory/$sample\L001_R2_001_3trimmed.fastq.gz  \
-    $fastq_directory/$sample\L001_R1_001.fastq.gz  $fastq_directory/$sample\L001_R2_001.fastq.gz \
+    -o $fastq_directory/$sample\1_3trimmed.fastq.gz \
+    -p $fastq_directory/$sample\2_3trimmed.fastq.gz  \
+    $fastq_directory/$sample\1.fastq.gz  $fastq_directory/$sample\2.fastq.gz \
     --minimum-length 40 \
     > $output_dir/$sample\_cutadapt_report.txt &&
 trimmomatic PE \
     -threads 4 \
     -trimlog $output_dir/$sample\trim \
-    $fastq_directory/$sample\L001_R1_001_3trimmed.fastq.gz $fastq_directory/$sample\L001_R2_001_3trimmed.fastq.gz \
-    $fastq_directory/$sample\L001_R1_001_3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R1_001_3trimmed_q20_un.fastq.gz $fastq_directory/$sample\L001_R2_001_3trimmed_q20.fastq.gz  $fastq_directory/$sample\L001_R2_001_3trimmed_q20_un.fastq.gz \
+    $fastq_directory/$sample\1_3trimmed.fastq.gz $fastq_directory/$sample\2_3trimmed.fastq.gz \
+    $fastq_directory/$sample\1_3trimmed_q20.fastq.gz   $fastq_directory/$sample\1_3trimmed_q20_un.fastq.gz $fastq_directory/$sample\2_3trimmed_q20.fastq.gz  $fastq_directory/$sample\2_3trimmed_q20_un.fastq.gz \
     SLIDINGWINDOW:4:20 \
     MINLEN:40 &&
-fastqc $fastq_directory/$sample\L001_R1_001_3trimmed_q20.fastq.gz \
+fastqc $fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
     --outdir $output_dir &&
-fastqc $fastq_directory/$sample\L001_R2_001_3trimmed_q20.fastq.gz \
+fastqc $fastq_directory/$sample\2_3trimmed_q20.fastq.gz \
     --outdir $output_dir &&
-megahit -1 $fastq_directory/$sample\L001_R1_001_3trimmed_q20.fastq.gz -2 $fastq_directory/$sample\L001_R2_001_3trimmed_q20.fastq.gz -o $output_dir/$sample\_Megahit_readassembly  \
+megahit -1 $fastq_directory/$sample\1_3trimmed_q20.fastq.gz -2 $fastq_directory/$sample\2_3trimmed_q20.fastq.gz -o $output_dir/$sample\_Megahit_readassembly  \
+spades.py -1 $fastq_directory/$sample\1_3trimmed_q20.fastq.gz -2 $fastq_directory/$sample\1_3trimmed_q20.fastq.gz -o $output_dir/$sample\_Megahit_readassembly \
 ;done
