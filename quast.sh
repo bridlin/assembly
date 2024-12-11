5e15d7e3192f85f24bc6f1e24efe015e5c7d4b5b
bridlin <bridlinbarckmann@gmail.com>
2024-12-11 11:56:47 +0100

    correction typo

--- quast.sh
+++ quast.sh
@@ -48,11 +48,12 @@ source assembly/config.txt
 for sample in "${input_list[@]}"; do
     echo $sample &&
     quast.py \
-    assembly_output/pilon/$sample\_pilon_mh_1.fasta \
-    assembly_output/pilon/$sample\_pilon_mh_2.fasta \
-    assembly_output/pilon/$sample\_pilon_mh_3.fasta \
-    assembly_output/pilon/$sample\_pilon_mh_4.fasta \
-    assembly_output/pilon/$sample\_pilon_mh_5.fasta
+        assembly_output/$sample\_Spades_readassembly/scaffolds.fasta \
+        assembly_output/pilon/$sample\_pilon_mh_1.fasta \
+        assembly_output/pilon/$sample\_pilon_mh_2.fasta \
+        assembly_output/pilon/$sample\_pilon_mh_3.fasta \
+        assembly_output/pilon/$sample\_pilon_mh_4.fasta \
+        assembly_output/pilon/$sample\_pilon_mh_5.fasta \
         -r ../chu_diag_microbiom_setup/genome/TriTrypDB-68_LmexicanaMHOMGT2001U1103/TriTrypDB-68_LmexicanaMHOMGT2001U1103_Genome.fasta \
         -g ../chu_diag_microbiom_setup/genome/TriTrypDB-68_LmexicanaMHOMGT2001U1103/TriTrypDB-68_LmexicanaMHOMGT2001U1103_Orf50.gff \
         -1 assembly_fastq/$sample\1_3trimmed_q20.fastq.gz -2 assembly_fastq/$sample\2_3trimmed_q20.fastq.gz \
