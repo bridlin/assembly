7f1d29cc6905661e2dd8626751016b17308f5985
bridlin <bridlinbarckmann@gmail.com>
2024-12-12 14:55:23 +0100

    added line conmment

--- assembly_pilon.sh
+++ assembly_pilon.sh
@@ -30,17 +30,17 @@ bowtie2-build \
 
 for sample in "${input_list[@]}"; do
 echo $sample &&
-bowtie2 \
-    -x $genome \
-    -1 $fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
-    -2 $fastq_directory/$sample\2_3trimmed_q20.fastq.gz   \
-    -S $output_dir/bowtie2/$sample\aln-pe.sam \
-    2> $output_dir/bowtie2/$sample\_bowtie.log &&
-samtools view -S -b $output_dir/bowtie2/$sample\aln-pe\.sam > $output_dir/bowtie2/$sample\aln-pe\.sam.bam &&
-samtools sort $output_dir/bowtie2/$sample\aln-pe\.sam.bam -o $output_dir/bowtie2/$sample\aln-pe\_sorted.bam &&
-samtools index $output_dir/bowtie2/$sample\aln-pe\_sorted.bam &&
-rm -f  $output_dir/bowtie2/$sample\aln-pe\.sam &&
-rm -f  $output_dir/bowtie2/$sample\aln-pe\.sam.bam &&
+# bowtie2 \
+#     -x $genome \
+#     -1 $fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
+#     -2 $fastq_directory/$sample\2_3trimmed_q20.fastq.gz   \
+#     -S $output_dir/bowtie2/$sample\aln-pe.sam \
+#     2> $output_dir/bowtie2/$sample\_bowtie.log &&
+# samtools view -S -b $output_dir/bowtie2/$sample\aln-pe\.sam > $output_dir/bowtie2/$sample\aln-pe\.sam.bam &&
+# samtools sort $output_dir/bowtie2/$sample\aln-pe\.sam.bam -o $output_dir/bowtie2/$sample\aln-pe\_sorted.bam &&
+# samtools index $output_dir/bowtie2/$sample\aln-pe\_sorted.bam &&
+# rm -f  $output_dir/bowtie2/$sample\aln-pe\.sam &&
+# rm -f  $output_dir/bowtie2/$sample\aln-pe\.sam.bam &&
 pilon \
     -Xmx100g \
     --genome $genome \
@@ -54,29 +54,29 @@ pilon \
     --fix snps,indels \
 ; done
 
-bwa index $genome 
+# bwa index $genome 
 
-for sample in "${input_list[@]}"; do
-echo $sample &&
- bwa-mem2 \
-    mem $genome \  
-    $fastq_directory/$sample\1_3trimmed_q20.fastq.gz\
-    $fastq_directory/$sample\2_3trimmed_q20.fastq.gz \
-    > $output_dir/bwa/$sample\aln-pe.sam &&     
-samtools view -S -b $output_dir/bwa/$sample\aln-pe\.sam > $output_dir/bwa/$sample\aln-pe\.sam.bam &&
-samtools sort $output_dir/bwa/$sample\aln-pe\.sam.bam -o $output_dir/bwa/$sample\aln-pe\_sorted.bam &&
-samtools index $output_dir/bwa/$sample\aln-pe\_sorted.bam &&
-rm -f  $output_dir/bwa/$sample\aln-pe\.sam &&
-rm -f  $output_dir/bwa/$sample\aln-pe\.sam.bam &&
-pilon \
-    -Xmx100g \
-    --genome $genome \
-    --bam $output_dir/bwa/$sample\aln-pe\_sorted.bam \
-    --output $sample\_bwa_pilon_assembly \
-    --outdir $output_dir/pilon_assembly \
-    --threads 4  \
-    --changes \
-    --tracks \
-    --vcf \
-    --fix snps,indels \
-; done
+# for sample in "${input_list[@]}"; do
+# echo $sample &&
+#  bwa-mem2 \
+#     mem $genome \  
+#     $fastq_directory/$sample\1_3trimmed_q20.fastq.gz\
+#     $fastq_directory/$sample\2_3trimmed_q20.fastq.gz \
+#     > $output_dir/bwa/$sample\aln-pe.sam &&     
+# samtools view -S -b $output_dir/bwa/$sample\aln-pe\.sam > $output_dir/bwa/$sample\aln-pe\.sam.bam &&
+# samtools sort $output_dir/bwa/$sample\aln-pe\.sam.bam -o $output_dir/bwa/$sample\aln-pe\_sorted.bam &&
+# samtools index $output_dir/bwa/$sample\aln-pe\_sorted.bam &&
+# rm -f  $output_dir/bwa/$sample\aln-pe\.sam &&
+# rm -f  $output_dir/bwa/$sample\aln-pe\.sam.bam &&
+# pilon \
+#     -Xmx100g \
+#     --genome $genome \
+#     --bam $output_dir/bwa/$sample\aln-pe\_sorted.bam \
+#     --output $sample\_bwa_pilon_assembly \
+#     --outdir $output_dir/pilon_assembly \
+#     --threads 4  \
+#     --changes \
+#     --tracks \
+#     --vcf \
+#     --fix snps,indels \
+# ; done
