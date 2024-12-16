002edb1263ec61e0a2daa96d3009e84ecba5fde3
bridlin <bridlinbarckmann@gmail.com>
2024-12-16 13:45:55 +0100

    indentation error

--- dup_removal.sh
+++ dup_removal.sh
@@ -23,29 +23,29 @@ output_dir=assembly_output_2
 mkdir -p $output_dir
 
 for sample in "${input_list[@]}"; do
-cutadapt  -a CTGTCTCTTATACACATCT+ATGTGTATAAGAGACA -A CTGTCTCTTATACACATCT+ATGTGTATAAGAGACA \
-    -o $fastq_directory/$sample\1_3trimmed.fastq.gz \
-    -p $fastq_directory/$sample\2_3trimmed.fastq.gz  \
-    $fastq_directory/$sample\1.fastq.gz  $fastq_directory/$sample\2.fastq.gz \
-    --minimum-length 40 \
-    > $output_dir/$sample\_cutadapt_report.txt &&
-fastqc $fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
-    --outdir $output_dir &&
-fastqc $fastq_directory/$sample\2_3trimmed_q20.fastq.gz \
-    --outdir $output_dir &&
-### clumping reads
-clumpify.sh \
-    in1=$fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
-    in2=$fastq_directory/$sample\2_3trimmed_q20.fastq.gz \
-    out1=$fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
-    out2=$fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
-    dedupe=t \
-    optical=f \
-    outd=$fastq_directory/$sample\duplicates.fq &&
-fastqc $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
-    --outdir $output_dir &&
-fastqc $fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
-    --outdir $output_dir 
+    cutadapt  -a CTGTCTCTTATACACATCT+ATGTGTATAAGAGACA -A CTGTCTCTTATACACATCT+ATGTGTATAAGAGACA \
+        -o $fastq_directory/$sample\1_3trimmed.fastq.gz \
+        -p $fastq_directory/$sample\2_3trimmed.fastq.gz  \
+        $fastq_directory/$sample\1.fastq.gz  $fastq_directory/$sample\2.fastq.gz \
+        --minimum-length 40 \
+        > $output_dir/$sample\_cutadapt_report.txt &&
+    fastqc $fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
+        --outdir $output_dir &&
+    fastqc $fastq_directory/$sample\2_3trimmed_q20.fastq.gz \
+        --outdir $output_dir &&
+    ### clumping reads
+    clumpify.sh \
+        in1=$fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
+        in2=$fastq_directory/$sample\2_3trimmed_q20.fastq.gz \
+        out1=$fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
+        out2=$fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
+        dedupe=t \
+        optical=f \
+        outd=$fastq_directory/$sample\duplicates.fq &&
+    fastqc $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
+        --outdir $output_dir &&
+    fastqc $fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
+        --outdir $output_dir 
 ;done
 
 multiqc   \
