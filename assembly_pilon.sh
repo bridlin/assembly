98c17ad83bb5f4122bb40f6c30ac470f8eac7d73
bridlin <bridlinbarckmann@gmail.com>
2024-12-17 12:12:49 +0100

    changes in pilon params

--- assembly_pilon.sh
+++ assembly_pilon.sh
@@ -30,7 +30,7 @@ echo "test4"
 
 output_bwa=$output_dir\_bwa 
 output_bowtie=$output_dir\_bowtie2
-output_pilon=$output_dir\_pilon-assembly
+output_pilon=$output_dir\_pilon-assembly_fix-all
 
 mkdir -p $output_bwa
 mkdir -p $output_bowtie
@@ -46,17 +46,17 @@ echo $genome
 for sample in "${input_list[@]}"; do
 echo "test6"
 echo $sample &&
-bowtie2 \
-    -x $genome \
-    -1 $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
-    -2 $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz   \
-    -S $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped.sam \
-    2> $output_bowtie/$sample\_bowtie_3trimmed_q20_clumped.log &&
-samtools view -S -b $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam > $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
-samtools sort $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam -o $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
-samtools index $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
-rm -f  $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam &&
-rm -f  $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
+# bowtie2 \
+#     -x $genome \
+#     -1 $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
+#     -2 $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz   \
+#     -S $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped.sam \
+#     2> $output_bowtie/$sample\_bowtie_3trimmed_q20_clumped.log &&
+# samtools view -S -b $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam > $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
+# samtools sort $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam -o $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
+# samtools index $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
+# rm -f  $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam &&
+# rm -f  $output_bowtie/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
 pilon \
     -Xmx32g \
     --genome $genome \
@@ -67,21 +67,21 @@ pilon \
     --changes \
     --tracks \
     --vcf \
-    --fix snps,indels \
+    #--fix snps,indels \
 ; done
 
 # bwa-mem2 index  -p TriTrypDB-68_LinfantumJPCM5_Genome $genome
 
 for sample in "${input_list[@]}"; do
 echo $sample &&
-bwa-mem2 \
-    mem $genome  $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz  $fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
-    > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped.sam &&     
-samtools view -S -b $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
-samtools sort $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam -o $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
-samtools index $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
-rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam &&
-rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
+# bwa-mem2 \
+#     mem $genome  $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz  $fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
+#     > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped.sam &&     
+# samtools view -S -b $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
+# samtools sort $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam -o $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
+# samtools index $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
+# rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam &&
+# rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
 pilon \
     -Xmx100g \
     --genome $genome \
@@ -92,5 +92,5 @@ pilon \
     --changes \
     --tracks \
     --vcf \
-    --fix snps,indels \
+    #--fix snps,indels \
 ; done
