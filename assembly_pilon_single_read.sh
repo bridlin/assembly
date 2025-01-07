2567775598926d8bd96d4a0189b4febdda2c4c5e
bridlin <bridlinbarckmann@gmail.com>
2025-01-07 11:22:53 +0100

    added option to assemble singel reads

---
+++ assembly_pilon_single_read.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user bridlin.barckmann@umontpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpusr-task 4
+#SBATCH --mem  128GB
+
+echo "test"
+module load bowtie2/2.5.1
+module load samtools/1.21
+module load pilon/1.23
+module load bwa-mem2/2.2.1
+
+echo "test2"
+
+source assembly/config.txt
+
+echo "test3"
+
+fastq_directory=$run\_fastq
+output_dir=$run\_output_nexteratrim_clumped
+genome='../chu_diag_microbiom_setup/genome/TriTrypDB-68_LinfantumJPCM5/TriTrypDB-68_LinfantumJPCM5_Genome.fasta'
+
+echo "test4"
+
+output_bwa=$output_dir\_bwa 
+output_bowtie=$output_dir\_bowtie2_2
+output_pilon=$output_dir\_pilon-assembly_fix-all
+
+mkdir -p $output_bwa
+mkdir -p $output_bowtie
+mkdir -p $output_pilon
+
+echo "test5"
+
+
+echo $genome
+
+bowtie2-build -f $genome $genome 
+
+for sample in "${input_list[@]}"; do
+echo "test6"
+echo $sample &&
+bowtie2 \
+    -x $genome \
+    -U $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
+    -S $output_bowtie/$sample\aln_3trimmed_q20_clumped.sam \
+    2> $output_bowtie/$sample\_bowtie_3trimmed_q20_clumped.log &&
+samtools view -S -b $output_bowtie/$sample\aln_3trimmed_q20_clumped\.sam > $output_bowtie/$sample\aln_3trimmed_q20_clumped\.sam.bam &&
+samtools sort $output_bowtie/$sample\aln_3trimmed_q20_clumped\.sam.bam -o $output_bowtie/$sample\aln_3trimmed_q20_clumped\_sorted.bam &&
+samtools index $output_bowtie/$sample\aln_3trimmed_q20_clumped\_sorted.bam &&
+rm -f  $output_bowtie/$sample\aln_3trimmed_q20_clumped\.sam &&
+rm -f  $output_bowtie/$sample\aln_3trimmed_q20_clumped\.sam.bam &&
+pilon \
+    -Xmx32g \
+    --genome $genome \
+    --bam $output_bowtie/$sample\aln_3trimmed_q20_clumped\_sorted.bam \
+    --output $sample\_bowtie_3trimmed_q20_clumped_pilon_assembly \
+    --outdir $output_pilon \
+    --threads 4  \
+    --changes \
+    --tracks \
+    --vcf \
+; done
+
+bwa-mem2 index  -p TriTrypDB-68_LinfantumJPCM5_Genome $genome
+
+for sample in "${input_list[@]}"; do
+echo $sample &&
+bwa-mem2 \
+    mem $genome  $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz   \
+    > $output_bwa/$sample\aln_3trimmed_q20_clumped.sam &&     
+samtools view -S -b $output_bwa/$sample\aln_3trimmed_q20_clumped\.sam > $output_bwa/$sample\aln_3trimmed_q20_clumped\.sam.bam &&
+samtools sort $output_bwa/$sample\aln_3trimmed_q20_clumped\.sam.bam -o $output_bwa/$sample\aln_3trimmed_q20_clumped\_sorted.bam &&
+samtools index $output_bwa/$sample\aln_3trimmed_q20_clumped\_sorted.bam &&
+rm -f  $output_bwa/$sample\aln_3trimmed_q20_clumped\.sam &&
+rm -f  $output_bwa/$sample\aln_3trimmed_q20_clumped\.sam.bam &&
+pilon \
+    -Xmx100g \
+    --genome $genome \
+    --bam $output_bwa/$sample\aln_3trimmed_q20_clumped\_sorted.bam \
+    --output $sample\_bwa_3trimmed_q20_clumped_pilon_assembly \
+    --outdir $output_pilon \
+    --threads 4  \
+    --changes \
+    --tracks \
+    --vcf \
+; done
---
+++ single_read_assembley.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-type END
+#SBATCH --mail-user b-barckmann@chu-montpellier.fr
+#
+#
+#SBATCH --partition fast
+#SBATCH --cpus-per-task 4
+#SBATCH --mem  128GB
+
+
+module load cutadapt/4.0
+module load trimmomatic/0.39
+module load fastqc/0.11.9
+module load megahit/1.2.9  
+module load velvet/1.2.10
+module load spades/3.15.5
+module load multiqc/1.13
+module load bbmap/39.00
+
+source assembly/config.txt
+
+fastq_directory=$run\_fastq
+output_dir=$run\_output_3
+
+mkdir $output_dir
+
+
+for sample in "${input_list[@]}"; do
+echo $sample &&
+fastqc $fastq_directory/$sample\1.fastq.gz \
+    --outdir $output_dir &&
+### cutadapt to trimm Nextra transposase adapters
+cutadapt  -a CTGTCTCTTATACACATCT -a AGATGTGTATAAGAGACAG  \
+    -o $fastq_directory/$sample\1_3trimmed.fastq.gz \
+    $fastq_directory/$sample\1.fastq.gz  \
+    --minimum-length 40 \
+    > $output_dir/$sample\_cutadapt_report.txt &&
+trimmomatic PE \
+    -threads 4 \
+    -trimlog $output_dir/$sample\trim \
+    $fastq_directory/$sample\1_3trimmed.fastq.gz  \
+    $fastq_directory/$sample\1_3trimmed_q20.fastq.gz   $fastq_directory/$sample\1_3trimmed_q20_un.fastq.gz \
+    SLIDINGWINDOW:4:20 \
+    MINLEN:40 &&
+fastqc $fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
+    --outdir $output_dir &&
+### clumping reads
+    clumpify.sh \
+        in=$fastq_directory/$sample\1_3trimmed_q20.fastq.gz \
+        out=$fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
+        dedupe=t \
+        optical=f &&
+fastqc $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
+    --outdir $output_dir &&
+## assembly
+megahit --read $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz  -o $output_dir/$sample\_Megahit_readassembly_clumped  \
+# spades.py --isolate -1 $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz -2 $fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz -o $output_dir/$sample\_Spades_readassembly_clumped \
+;done
+
+multiqc   \
+    $output_dir \
+    --outdir $output_dir  
