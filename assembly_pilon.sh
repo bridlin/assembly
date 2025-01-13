46c9c37e9b311d89f397988d021be4b52e75cb67
bridlin <bridlinbarckmann@gmail.com>
2025-01-13 11:38:04 +0100

    line comment and config

--- assembly_pilon.sh
+++ assembly_pilon.sh
@@ -74,14 +74,14 @@ echo $genome
 
 for sample in "${input_list[@]}"; do
 echo $sample &&
-# bwa-mem2 \
-#     mem $genome  $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz  $fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
-#     > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped.sam &&     
-# samtools view -S -b $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
-# samtools sort $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam -o $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
-# samtools index $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
-# rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam &&
-# rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
+bwa-mem2 \
+    mem $genome  $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz  $fastq_directory/$sample\2_3trimmed_q20_clumped.fastq.gz \
+    > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped.sam &&     
+samtools view -S -b $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam > $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
+samtools sort $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam -o $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
+samtools index $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\_sorted.bam &&
+rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam &&
+rm -f  $output_bwa/$sample\aln-pe_3trimmed_q20_clumped\.sam.bam &&
 pilon \
     -Xmx100g \
     --genome $genome \
