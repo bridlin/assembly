e9161d717282810426a428a904f2348c57e59861
bridlin <bridlinbarckmann@gmail.com>
2025-02-05 13:17:18 +0100

    config and plasmid align changes

--- plasmid_align.sh
+++ plasmid_align.sh
@@ -17,14 +17,14 @@ module load  samtools/1.13
 source assembly/config.txt
 
 fastq_directory=$run\_fastq
-genome=plasmids/23pTB007_Cas9_T7_Tub.fa
-output=plasmid_mapping_23pTB007_Cas9_T7_Tub-f9
+genome=fasta_LinJPCM5_plus_plasmid/TriTrypDB-68_LinfantumJPCM5TriTrypDB-68_LinfantumJPCM5_Genome_plus_23pTB007_Cas9_T7_Tub.fasta
+output=Genome_plus_plasmid_mapping
 
 mkdir -p $output
 
 for sample in "${input_list[@]}"; do
-# bowtie2-build \
-#     -f $genome $genome &&
+bowtie2-build \
+    -f $genome $genome &&
 bowtie2 \
     -x $genome \
     -1 $fastq_directory/$sample\1_3trimmed_q20_clumped.fastq.gz \
